bb0f827d3f83d99264faa879440b69a9201adbcf
Clayton Daley <clayton.daley@gmail.com>
2020-07-01 11:02:26 -0500

    makes resource type explicit with instructions for changing it and explicit error handling; also uses relative import path to be insensitive to defined root

--- src/provider.py
+++ src/provider.py
@@ -1,9 +1,15 @@
 import os
 import logging
-import cfn_custom_provider
+from . import cfn_custom_provider
 
 logging.basicConfig(level=os.getenv('LOG_LEVEL', 'INFO'))
 
 
 def handler(request, context):
-    return cfn_custom_provider.handler(request, context)
+    # Modify this value to support a custom Custom:: tag
+    # Add elif conditions (and additional cfn_*_provider files to support multiple tags
+    # see binxio/cfn-certificate-provider for an example
+    if request["ResourceType"] == "Custom::Custom":
+        return cfn_custom_provider.handler(request, context)
+    else:
+        raise ValueError(f'No handler found for custom resource {request["ResourceType"]}')
